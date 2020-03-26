3ef65af8ec7973e1c6acb6dcb228a0e2fc2898a2
KosukeTakinami <zeal19300116@c-league.jp>
2020-03-26 12:07:30 +0900

    キャラクターセレクトステージにキャラクターの画像を表示してコントローラーもしくはキーボードで選択できるようにしました。

--- TGS/ProjectData/GameSources/CharSelect.cpp
+++ TGS/ProjectData/GameSources/CharSelect.cpp
@@ -19,7 +19,62 @@ namespace basecross {
 	}
 
 	void CharSelectStage::CreateUI() {
+		AddGameObject<UI_Character_Select_Static_Image>(
+			Vec2(1920.0f, 1080.0f),
+			Vec3(0.0f, 0.0f, 0.0f),
+			Vec3(1.0f, 1.0f, 1.0f),
+			1,
+			Col4(1.0f, 1.0f, 1.0f, 1.0f),
+			m_baseImageName
+			);
 
+		m_chicken = AddGameObject<UI_Character_Select_Static_Image>(
+			Vec2(1920.0f, 360.0f),
+			Vec3(0.0f, -360.0f, 0.0f),
+			Vec3(1.0f, 1.0f, 1.0f),
+			1,
+			Col4(1.0f, 1.0f, 1.0f, 1.0f),
+			m_chickenImageName
+			);
+
+		m_potato = AddGameObject<UI_Character_Select_Static_Image>(
+			Vec2(1920.0f, 360.0f),
+			Vec3(0.0f, -360.0f, 0.0f),
+			Vec3(1.0f, 1.0f, 1.0f),
+			1,
+			Col4(1.0f, 1.0f, 1.0f, 1.0f),
+			m_potatoImageName
+			);
+
+		m_shrimp = AddGameObject<UI_Character_Select_Static_Image>(
+			Vec2(1920.0f, 360.0f),
+			Vec3(0.0f, -360.0f, 0.0f),
+			Vec3(1.0f, 1.0f, 1.0f),
+			1,
+			Col4(1.0f, 1.0f, 1.0f, 1.0f),
+			m_shrimpImageName
+			);
+
+		m_doughnut = AddGameObject<UI_Character_Select_Static_Image>(
+			Vec2(1920.0f, 360.0f),
+			Vec3(0.0f, -360.0f, 0.0f),
+			Vec3(1.0f, 1.0f, 1.0f),
+			1,
+			Col4(1.0f, 1.0f, 1.0f, 1.0f),
+			m_doughnutImageName
+			);
+
+		m_mask = AddGameObject<UI_Character_Select_Mask_Image>(
+			Vec2(3360.0f, 720.0),
+			Vec3(-720.0f + (480.0f * 0), 180.0f, 0.0f),
+			Vec3(1.0f, 1.0f, 1.0f),
+			1,
+			Col4(1.0f, 1.0f, 1.0f, 1.0f),
+			m_maskImageName,
+			m_startIndex
+			);
+
+		SetStatusImage(m_startIndex);
 	}
 
 	void CharSelectStage::OnCreate() {
@@ -33,7 +88,43 @@ namespace basecross {
 	}
 
 	void CharSelectStage::OnUpdate() {
-
+		SetStatusImage(m_mask->GetIndex());
+		Select();
 	}
 
+	void CharSelectStage::SetStatusImage(int index) {
+		m_chicken->SetDrawActive(false);
+		m_potato->SetDrawActive(false);
+		m_shrimp->SetDrawActive(false);
+		m_doughnut->SetDrawActive(false);
+		switch (index)
+		{
+		case 0:
+			m_potato->SetDrawActive(true);
+			break;
+		case 1:
+			m_shrimp->SetDrawActive(true);
+			break;
+		case 2:
+			m_chicken->SetDrawActive(true);
+			break;
+		case 3:
+			m_doughnut->SetDrawActive(true);
+			break;
+		default:
+			break;
+		}
+	}
+
+	void CharSelectStage::Select() {
+		auto KeyState = App::GetApp()->GetInputDevice().GetKeyState();//コントローラの取得
+		auto cntlVec = App::GetApp()->GetInputDevice().GetControlerVec();
+		WORD wButtons = 0;
+		if (KeyState.m_bPressedKeyTbl[VK_SPACE] || cntlVec[0].wPressedButtons & XINPUT_GAMEPAD_A) {
+			//ここにゲームステージへ遷移する処理を書く
+			//GetIndex()で選択したキャラクターのIndexを取得できます
+			int index = GetIndex();
+			App::GetApp()->GetScene<Scene>()->SetGameStage(GameStageKey::game);
+		}
+	}
 }
