811f46de46c75fb40084a9361083f2df5ccb0146
KosukeTakinami <zeal19300116@c-league.jp>
2020-03-30 14:16:29 +0900

    ポーズ画面の作成

--- TGS/ProjectData/GameSources/MyUI.h
+++ TGS/ProjectData/GameSources/MyUI.h
@@ -37,9 +37,57 @@ namespace basecross {
 
 	};
 
-	class UI_Character_Select_Static_Image :public UI_Base {
+
+	//------------------------------------------------------------------------------------------------
+	//テキスト : Class
+	//------------------------------------------------------------------------------------------------
+
+	class UI_Sprite_Text :public GameObject {
+		wstring m_fontName;
+		float m_fontSize;
+		Col4 m_color;
+		Rect2D<float> m_rect;
+		StringSprite::TextAlignment m_textAlignment;
+		wstring m_text;
+		int m_layer;
+		bool m_isBk;
+
 	public:
-		UI_Character_Select_Static_Image(const shared_ptr<Stage>& StagePtr,
+		UI_Sprite_Text(const shared_ptr<Stage>& StagePtr,
+			const wstring& fontName,
+			const float fontSize,
+			const Col4& color,
+			const Rect2D<float>& rect,
+			const StringSprite::TextAlignment textAlignment,
+			const wstring& text,
+			const int layer,
+			const bool isBk
+		) :
+			GameObject(StagePtr),
+			m_fontName(fontName),
+			m_fontSize(fontSize),
+			m_color(color),
+			m_rect(rect),
+			m_textAlignment(textAlignment),
+			m_text(text),
+			m_layer(layer),
+			m_isBk(isBk)
+		{}
+		~UI_Sprite_Text() {}
+
+		virtual void OnCreate() override;
+
+		void Remove();
+	};
+
+
+	//------------------------------------------------------------------------------------------------
+	//スタティックな画像 : Class
+	//------------------------------------------------------------------------------------------------
+
+	class UI_Static_Image :public UI_Base {
+	public:
+		UI_Static_Image(const shared_ptr<Stage>& StagePtr,
 			const Vec2& vertex,
 			const Vec3& pos,
 			const Vec3& scale,
@@ -57,10 +105,15 @@ namespace basecross {
 				textures
 			)
 		{}
-		~UI_Character_Select_Static_Image() {}
+		~UI_Static_Image() {}
 
 		virtual void OnCreate() override;
 	};
+
+
+	//------------------------------------------------------------------------------------------------
+	//キャラクターセレクトの選択マスク : Class
+	//------------------------------------------------------------------------------------------------
 
 	class UI_Character_Select_Mask_Image :public UI_Base {
 		int m_indexMin;
@@ -106,6 +159,160 @@ namespace basecross {
 
 		int GetIndex() {
 			return m_maskIndex;
+		}
+	};
+
+
+	//------------------------------------------------------------------------------------------------
+	//キャラクターステータス : 構造体
+	//------------------------------------------------------------------------------------------------
+
+	typedef struct CharacterStatus {
+		wstring playerName;
+		int kill;
+		int death;
+		int score;
+		bool isPlayer;
+		int unique;
+	} CharacterStatus_s;
+
+
+	//------------------------------------------------------------------------------------------------
+	//キル詳細 : 構造体
+	//------------------------------------------------------------------------------------------------
+
+	typedef struct CharacterKillDetails {
+		wstring killName;
+		int level;
+	} CharacterKillDetails_s;
+
+
+	//------------------------------------------------------------------------------------------------
+	//タブ : Class
+	//------------------------------------------------------------------------------------------------
+
+	class UI_Tab: public GameObject {
+		int m_indexMin;
+		int m_indexMax;
+		float m_movingValue;
+		float m_startPosX;
+		int m_maskIndex;
+
+		float m_space;
+
+		float m_oldFThumbLX;
+
+		wstring m_tabImageName;
+		wstring m_tabMaskImageName;
+
+		std::shared_ptr<UI_Static_Image> m_tab;
+		vector<std::shared_ptr<UI_Static_Image>> m_masks;
+
+		Vec3 m_pos;
+
+		void CreateTab();
+		void CreateMasks();
+
+	public:
+		UI_Tab(const shared_ptr<Stage>& StagePtr,
+			const Vec3& pos) :
+			GameObject(StagePtr),
+			m_pos(pos),
+			m_indexMin(0),
+			m_indexMax(3),
+			m_movingValue(App::GetApp()->GetGameWidth() / (m_indexMax + 1.0f)),
+			m_startPosX(0.0f),
+			m_maskIndex(0),
+			m_oldFThumbLX(0.0f),
+			m_tabImageName(L"PS_Tab.png"),
+			m_tabMaskImageName(L"PS_Tab_Mask.png")
+		{}
+		~UI_Tab() {}
+
+		virtual void OnCreate() override;
+		virtual void OnUpdate() override;
+
+		void MaskMove();
+		void SetMaskIndex(int& index);
+
+		int GetIndex() {
+			return m_maskIndex;
+		}
+	};
+
+
+	//------------------------------------------------------------------------------------------------
+	//ポーズ画面 : Class
+	//------------------------------------------------------------------------------------------------
+
+	class UI_The_World :public GameObject {
+		wstring m_fontName;
+
+		wstring m_tableGroupName;
+
+		wstring m_baseImageName;
+		wstring m_separator1pxImageName;
+		wstring m_separator3pxImageName;
+
+		wstring m_playerBadgeImageName;
+
+		std::shared_ptr<UI_Static_Image> m_base;
+
+		vector<CharacterStatus_s> m_characterStatuses;
+		vector<CharacterKillDetails_s> m_characterStatusKillDetails;
+
+		std::shared_ptr<UI_Sprite_Text> m_title;
+
+		int m_index;
+
+		bool m_isShow;
+
+		Col4 m_white;
+
+		void Hide(bool e);
+		void ShowTitle(bool e);
+		void ShowTable(bool e);
+
+		void ChangeTab(int index);
+
+		void CreateTableHeader();
+
+		void CreateTableLine(CharacterStatus_s &status, int index);
+		void RemoveTableLine();
+
+	public:
+		UI_The_World(const shared_ptr<Stage>& StagePtr
+		):
+			GameObject(StagePtr),
+			m_fontName(L"メイリオ"),
+			m_tableGroupName(L"PauseTableGroup"),
+			m_characterStatuses(vector<CharacterStatus_s>(0)),
+			m_characterStatusKillDetails(vector<CharacterKillDetails_s>(0)),
+			m_isShow(false),
+			m_baseImageName(L"PS_Base.png"),
+			m_separator1pxImageName(L"PS_Separator_1px.png"),
+			m_separator3pxImageName(L"PS_Separator_3px.png"),
+			m_playerBadgeImageName(L"PS_PlayerBadge.png"),
+			m_base(NULL),
+			m_title(NULL),
+			m_index(0),
+			m_white(Col4(1.0f, 1.0f, 1.0f, 1.0f))
+		{}
+		~UI_The_World() {}
+
+		virtual void OnCreate() override;
+
+		void SetCharacterStatuses(const vector<CharacterStatus_s>& characterStatuses) {
+			m_characterStatuses = characterStatuses;
+		}
+		void SetCharacterStatus(const CharacterStatus_s& status);
+		void SetCharacterKillDetails(const vector<CharacterKillDetails_s>& characterStatusKillDetails) {
+			m_characterStatusKillDetails = characterStatusKillDetails;
+		}
+		void Show(bool e);
+
+		bool GetShowing() {
+			return m_isShow;
 		}
 	};
 }
